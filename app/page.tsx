ce9e07c3b21211a33a591382b85513cb9688f0de
wajahatalee <wajahatali.comsats@gmail.com>
2024-01-22 12:52:36 +0500

    Logic base and UI implementations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 import LotteryList from "./components/LotteryList";
+import menu from "../public/menu.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Welcome to Sample Lottery</h2>
+    <div className="container">
+      <header className="header">
+        <div className="menu-icon">
+          <Image src={menu} alt={"menu icon"} height={40} width={40} />
+        </div>
+        <div className="heading">
+          <h1 className="main-heading">Lottery</h1>
+        </div>
+      </header>
+      {/* <h2>Welcome to Sample Lottery</h2> */}
+      <div className="main-content">
+        <h3>Latest Results</h3>
+      </div>
       <LotteryList />
     </div>
   );
