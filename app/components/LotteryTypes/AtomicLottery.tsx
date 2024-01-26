a225f8125c0e8f4532e1c77cf318c33c2c34140c
wajahatalee <wajahatali.comsats@gmail.com>
2024-01-26 15:08:08 +0500

    Displays

--- app/components/LotteryTypes/AtomicLottery.tsx
+++ app/components/LotteryTypes/AtomicLottery.tsx
@@ -1,10 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../../styles/components/AtomicLottery.module.css";
+import Image from "next/image";
+import atomic from "../../../public/Logo_Atomic.png";
+import magnifier from "../../../public/Magnifier_Plusblue.png";
+import time from "../../../public/timeIcon.png";
+import polygon from "../../../public/Polygon.png";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { GoogleSignInButton } from "../authButton";
 
 const AtomicLottery: React.FC<{ data: any }> = ({ data }) => {
-  // Implement the AtomicLottery component using the provided JSON data
+  const [isOpen, setIsOpen] = useState(false);
+
+  const session = useSession();
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  const handlePlayButtonClick = () => {
+    // Check if the user is authenticated before navigating to the play page
+    if (session.status === "loading") {
+      // Session status is still loading, do nothing or show a loading indicator
+      return;
+    }
+
+    if (!session || status === "unauthenticated") {
+      // Use Link for client-side navigation to the login page
+      // return (
+      //   <div>
+      //     <GoogleSignInButton />
+      //   </div>
+      // );
+      return (
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+      );
+    } else {
+      // Implement the logic to handle the play button click for authenticated users
+      console.log("User is authenticated. Implement play logic here.");
+    }
+  };
+
   return (
-    <div className="atomic-lottery">
-      {/* Add AtomicLottery component JSX here */}
+    <div className={styles.cosmicCard}>
+      <div className={styles.topBar}>
+        <div className={styles.leftBar}>
+          <Image src={atomic} alt={"Cosmic logo"} height={16} width={96} />
+          <p className={styles.lightBlueText}>No. {data.roundNumber}</p>
+        </div>
+        <Image src={magnifier} alt={"magnifier icon"} width={24} height={24} />
+      </div>
+
+      <div className={styles.winningNumber}>
+        <div className={styles.numberBall}>{data.previousWinningticket[0]}</div>
+        <div className={styles.numberBall}>{data.previousWinningticket[1]}</div>
+        <div className={styles.numberBall}>{data.previousWinningticket[2]}</div>
+        <div className={styles.numberBall}>{data.previousWinningticket[3]}</div>
+        <div className={styles.numberBall}>{data.previousWinningticket[4]}</div>
+        <div className={styles.colorBall}>{data.previousWinningticket[5]}</div>
+      </div>
+      <div className={styles.winningPot}>
+        <p className={styles.plaintext}>Winning Pot</p>
+        <div className={styles.amount}>
+          <h2 className={styles.prizeAmount}>{data.winningPot}</h2>Lucki
+        </div>
+      </div>
+      <div className={styles.footerStrip}>
+        <div className={styles.nextDraw}>
+          <p>Next Draw</p>
+          <Image src={time} alt={"remaining time"} width={18} height={18} />
+          {/* shall use a timeFormat functon to display this value more accurately */}
+          {data.nextDraw}:00:00
+        </div>
+        <button className={styles.play} onClick={handlePlayButtonClick}>
+          Play
+        </button>
+      </div>
+      <div
+        className={`${styles.status} ${isOpen ? styles.open : ""}`}
+        onClick={toggleDropdown}
+      >
+        {isOpen && (
+          <div className={styles.dropdownContent}>
+            <p>Current Pool: {data.currentPool}</p>
+            <p>Ticket count: {data.ticketCount}</p>
+          </div>
+        )}
+        <div className={styles.drop}>
+          <Image className={styles.iconPoly} src={polygon} alt="polygon icon" />
+          <span>{isOpen ? "Close" : "Current Pool Status"}</span>
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/LotteryTypes/ClassicLottery.tsx
+++ app/components/LotteryTypes/ClassicLottery.tsx
@@ -1,10 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../../styles/components/ClassicLottery.module.css";
+import Image from "next/image";
+import classic from "../../../public/Logo_Classic.png";
+import magnifier from "../../../public/Magnifier_Minus.png";
+import time from "../../../public/timeIcon.png";
+import polygon from "../../../public/Polygon.png";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { GoogleSignInButton } from "../authButton";
 
 const ClassicLottery: React.FC<{ data: any }> = ({ data }) => {
-  // Implement the ClassicLottery component using the provided JSON data
+  const [isOpen, setIsOpen] = useState(false);
+
+  const session = useSession();
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  const handlePlayButtonClick = () => {
+    // Check if the user is authenticated before navigating to the play page
+    if (session.status === "loading") {
+      // Session status is still loading, do nothing or show a loading indicator
+      return;
+    }
+
+    if (!session || status === "unauthenticated") {
+      // Use Link for client-side navigation to the login page
+      // return (
+      //   <div>
+      //     <GoogleSignInButton />
+      //   </div>
+      // );
+      return (
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+      );
+    } else {
+      // Implement the logic to handle the play button click for authenticated users
+      console.log("User is authenticated. Implement play logic here.");
+    }
+  };
+
   return (
-    <div className="classic-lottery">
-      {/* Add ClassicLottery component JSX here */}
+    <div className={styles.classicCard}>
+      <div className={styles.topBar}>
+        <div className={styles.leftBar}>
+          <Image src={classic} alt={"Classic logo"} height={16} width={96} />
+          <p className={styles.bluetext}>Past 5 Results</p>
+        </div>
+        <Image src={magnifier} alt={"magnifier icon"} width={24} height={24} />
+      </div>
+
+      <div className={styles.winningNumber}>
+        <div className={styles.numberRow}>
+          <div>{data.roundNumber}</div>
+          <div>{data.previousWinningticket[0]}</div>
+          <div>{data.winningPot}</div>
+        </div>
+        <div className={styles.numberRow}>
+          <div>{data.roundNumber}</div>
+          <div>{data.previousWinningticket[1]}</div>
+          <div>{data.winningPot}</div>
+        </div>
+        <div className={styles.numberRow}>
+          <div>{data.roundNumber}</div>
+          <div>{data.previousWinningticket[2]}</div>
+          <div>{data.winningPot}</div>
+        </div>
+        <div className={styles.numberRow}>
+          <div>{data.roundNumber}</div>
+          <div>{data.previousWinningticket[3]}</div>
+          <div>{data.winningPot}</div>
+        </div>
+        <div className={styles.numberRow}>
+          <div>{data.roundNumber}</div>
+          <div>{data.previousWinningticket[4]}</div>
+          <div>{data.winningPot}</div>
+        </div>
+      </div>
+      <div className={styles.footerStrip}>
+        <div className={styles.nextDraw}>
+          <span>Next Draw</span>
+          <Image src={time} alt={"remaining time"} width={18} height={18} />
+          {/* shall use a timeFormat functon to display this value more accurately */}
+          {data.nextDraw}:00:00
+        </div>
+        <button className={styles.play} onClick={handlePlayButtonClick}>
+          Play
+        </button>
+      </div>
+      <div
+        className={`${styles.status} ${isOpen ? styles.open : ""}`}
+        onClick={toggleDropdown}
+      >
+        {isOpen && (
+          <div className={styles.dropdownContent}>
+            <p>Current Pool: {data.currentPool}</p>
+            <p>Ticket count: {data.ticketCount}</p>
+          </div>
+        )}
+        <div className={styles.drop}>
+          <Image className={styles.iconPoly} src={polygon} alt="polygon icon" />
+          <span>{isOpen ? "Close" : "Current Pool Status"}</span>
+        </div>
+      </div>
     </div>
   );
 };
